8e91e569cbb156a6d8e76fd0beb011c81af632bc
Your name here <prisalunkhe002@gmail.com>
2023-01-19 17:42:54 +0530

    SB-1|Create new login api

--- API/index.js
+++ API/index.js
@@ -10,26 +10,43 @@ const PORT  =process.env.port;
 
 const mongoURL = process.env.DATABASE_URL;
 const databasePromise = mongoose.connect(mongoURL);
-
-var jsonParser = bodyParser.json();
+const jsonParser = bodyParser.json();
 
 //query with email is same email exist in user database
-app.post('/signup',jsonParser,async (req,res) => {
+app.post('/signup',jsonParser, async (req,res) => {
     const existingUser = await User.findOne({email:req.body.email})
     console.log(existingUser,"existingUser");
     if(existingUser){
         return res
         .send({error:"User already exists please login"})
-        .statusCode(400);
+        .status(400);
     }
     const authToken = uuidv4();
     try{
         const newUser = new User({...req.body, authToken : [authToken] });
     newUser.save();
-}catch(error){
+    }catch(error){
     console.log(error).status(400);
 }
 return res.send({msg:"User created successfully",authToken})
+});
+
+app.post("/login",jsonParser,async(req,res) =>{
+    const existingUser = await User.findOne({email:req.body.email});
+    if(!existingUser){
+        return res
+        .send({error:"user not found please enter valid details"})
+        .status(400);
+    }
+    if(existingUser.password !== req.body.password){
+        return res
+        .send({error:"Invalid password"})
+        .status(400);
+    }
+    const authToken = uuidv4();
+    existingUser.authToken.push(authToken);
+    await existingUser.save();
+    return res.send({ msg:"LogIn Sucessfully", authToken });
 });
 
 databasePromise.then(()=>{
